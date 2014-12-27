31f51d2a2a9ac931c1d4dc1540e93a89e8e7ae74
Joel <truesilver92@gmail.com>
2014-12-27 11:14:49 -0700

    added melpa repo to packege manager

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,8 @@
 ; init the package manager
 (when (>= emacs-major-version 24)
   (require 'package)
+  (add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (package-initialize)
 )
 
