98449616c5f8be257cf749dc606d8d1547110d3e
Joel Stoker <joel.d.stoker@gmail.com>
2016-01-04 19:24:28 -0700

    modified desktop save directories to work better with multiple projects

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,13 +13,13 @@
 (blink-cursor-mode 0)
 
 (desktop-save-mode 1)
-(setq desktop-restore-eager 10)
+(setq desktop-restore-eager 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(desktop-path (quote ("."))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
