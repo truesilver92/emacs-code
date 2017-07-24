715603215aba82f2f15b9bcc30a8d462e3e22afe
Joel Stoker <joel.d.stoker@gmail.com>
2017-07-24 15:02:24 -0600

    added yaml mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -93,6 +93,13 @@
                              (org-bullets-mode 1)
                              (auto-fill-mode))))
 
+(require 'yaml-mode)
+(progn
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-hook 'yaml-mode-hook
+            '(lambda ()
+               (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
+
 ;;(package-initialize)
 ;;(require 'go-mode-autoloads)
 
