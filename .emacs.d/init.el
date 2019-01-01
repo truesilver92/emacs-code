e9a978fe0800282d90849c074d3f0a6559f8e2d6
Joel Stoker <truesilver92@gmail.com>
2019-01-01 10:33:04 -0700

    removed the submodules

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,27 @@
+
+;; add melpa
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(eval-when-compile
+  (require 'use-package))
+
 ;; set character encoding
 (set-language-environment "UTF-8")
 
@@ -171,7 +195,7 @@
  '(org-export-backends (quote (ascii beamer html icalendar latex md odt org)))
  '(package-selected-packages
    (quote
-    (projectile php-mode paredit markdown-preview-mode go-mode geiser cider better-defaults))))
+    (use-package projectile php-mode paredit markdown-preview-mode go-mode geiser cider better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
