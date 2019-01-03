94721585cee665a542dcd9b2ef1a98eeebb47dfe
Joel Stoker <truesilver92@gmail.com>
2019-01-03 11:00:23 -0700

    added rjsx-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,7 +78,8 @@ There are two things you can do about this warning:
   :mode ("\\.ex\\'" "\\.exs\\'"))
 (use-package haskell-mode
   :mode "\\.hs\\'")
-(use-package jsx-mode)
+(use-package rjsx-mode
+  :hook "\\.js\\'")
 (use-package racket-mode
   :hook ((racket-mode racket-repl-mode) . racket-unicode-input-method-enable)
   :bind (("M-/" . complete-symbol)
@@ -143,7 +144,7 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-export-backends (quote (ascii beamer html icalendar latex md odt org))))
+ '(org-export-backends (quote (ascii beamer html icalendar latex md odt org)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
